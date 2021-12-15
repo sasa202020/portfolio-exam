062a95e4417e43654ccd1625432762183ad0f9a4
sasa202020 <sosiso4ki64@gmail.com>
2021-12-15 14:07:01 +0100

    made page 2 added files

---
+++ difprojects/pedal.js
@@ -0,0 +1,18 @@
+const products = [
+    {
+        id:0,
+        name: 'Classic Pedal',
+        price: 699.50,
+        instock: 10,
+        imgSrc: './img/pedalSink.jpg',
+        numberofUnits: 0,
+    },
+    {
+        id:1,
+        name: 'Sport pedal',
+        price: 1235.99,
+        instock: 10,
+        imgSrc: './img/sportPedal.jpg' ,
+        numberofUnits: 0,
+    },
+]
